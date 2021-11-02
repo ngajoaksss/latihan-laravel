e8d964a61dd45379df6362376da35fb942895784
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-02 15:06:09 +0700

    cok

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -32,9 +32,88 @@
            </span> 
 
         </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="box-count">
+                    <a href="https://sekolahpedia.id/school/sd">
+                        <div class="icon">
+                            <img src="https://sekolahpedia.id/template/images/icon/sd.png">
+                        </div>
+                        <h3 class="title">SD/MI</h3>
+                    </a>
+                    <a class="btn btn-outline-danger" href="https://sekolahpedia.id/school/sd">
+                        <i class="fa fa-angel-right">
+
+                        </i>
+                    </a>
+                </div>
+
+            </div>
+              <div class="col-md-3">
+                <div class="box-count">
+                    <a href="https://sekolahpedia.id/school/smp">
+                        <div class="icon">
+                            <img src="https://sekolahpedia.id/template/images/icon/smp.png">
+                        </div>
+                        <h3 class="title">SMP/MTs</h3>
+                    </a>
+                    <a class="btn btn-outline-danger" href="https://sekolahpedia.id/school/smp">
+                        <i class="fa fa-angel-right">
+                            
+                        </i>
+                    </a>
+                </div>
+
+            </div>
+            <div class="col-md-3">
+                <div class="box-count">
+                    <a href="https://sekolahpedia.id/school/sma">
+                        <div class="icon">
+                            <img src="https://sekolahpedia.id/template/images/icon/sma.png">
+                        </div>
+                        <h3 class="title">SMA/MA</h3>
+                    </a>
+                    <a class="btn btn-outline-danger" href="https://sekolahpedia.id/school/sma">
+                        <i class="fa fa-angel-right">
+                            
+                        </i>
+                    </a>
+                </div>
+
+            </div>
+            <div class="col-md-3">
+                <div class="box-count">
+                    <a href="https://sekolahpedia.id/school/smk">
+                        <div class="icon">
+                            <img src="https://sekolahpedia.id/template/images/icon/smk.png">
+                        </div>
+                        <h3 class="title">SMK/MAK</h3>
+                    </a>
+                    <a class="btn btn-outline-danger" href="https://sekolahpedia.id/school/smk">
+                        <i class="fa fa-angel-right">
+                            
+                        </i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
         
 
 </div>
+</section>
+<section class="section">
+    <div class="container">
+        <div class="titleTop">
+            <h3>
+                PPDB
+                <span>Online</span>
+            </h3>
+        </div>
+    </div>
+    <ul class="employerList">
+        <li></li>
+    </ul>
 </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>

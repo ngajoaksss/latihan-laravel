0196f694517019429fb5d37ce616cc3557ca89d9
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-02 10:41:37 +0700

    ini kerjain bagian view/main.blade.php nya aja

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+        
+});
+
+Route::get('/daftar-sekolah', function () {
+    return view('search');
 });

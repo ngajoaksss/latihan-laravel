a7a409b9875322934d76b2dbf7990c5e4f4ca395
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-02 14:14:46 +0700

    pusing

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,21 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="https://sekolahpedia.id/template/images/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
+    
 </head>
 <body>
     <section class = "home-section">
+        <div class="container">
         <div class="logo">
-            <img src="logosekolahpedia.png">
+            <img src="https://sekolahpedia.id/template/images/logosekolahpedia.png">
         </div>
         <div class="home-text">
-            
+            <h2 class="title">Daftar Sekolah Seluruh Indonesia</h2>
+            <p class="desc">
+                 <span class="prov">
+                    <span>34</span>
+                    provinsi
+                </span>
+            <span class="kota">
+                <span>514</span>
+                kota
+            </span>
+
         </div>
-    <h1>
-          {{-- buat judul --}}
-            
-    </h1>
+        
+
+</div>
 </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>

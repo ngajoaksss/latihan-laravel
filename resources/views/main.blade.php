7645c0d58e548a26b769cd87bde2dec8754e39fc
AlYgmi <nurrusulialadiyat@gmail.com>
2021-11-02 11:29:35 +0700

    jangar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,11 +8,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <section class = "home-section">
+        <div class="logo">
+            <img src="logosekolahpedia.png">
+        </div>
+        <div class="home-text">
+            
+        </div>
     <h1>
           {{-- buat judul --}}
             
     </h1>
+</section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>

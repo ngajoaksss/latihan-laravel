8e7d9f60c48e7412c24d5b567dc64d72f546ad17
ngajoaksss <93514929+ngajoaksss@users.noreply.github.com>
2021-11-03 14:05:37 +0700

    Update jual.blade.php

--- resources/views/jual.blade.php
+++ resources/views/jual.blade.php
@@ -1,6 +1,57 @@
 @extends('layouts.main')
 
 @section('container')
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Lisensi Office A1 365</h5>
+    <p class="card-text">SMK ANGKASA 1 MARGAHAYU</p>
+    <a href="#" class="btn btn-primary">Beli</a>
+  </div>
+</div>
 
-    
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Lisensi Mikrotik Level 4</h5>
+    <p class="card-text">SMK ANGKASA 1 MARGAHAYU</p>
+    <a href="#" class="btn btn-primary">Beli</a>
+  </div>
+  </div>
+  
+  <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">MIKROTIK RB 941-2ND Haplite </h5>
+    <p class="card-text">SMK ANGKASA 1 MARGAHAYU</p>
+    <a href="#" class="btn btn-primary">Beli</a>
+</div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">JASA SERVICE LAPTOP/PC dan HP</h5>
+    <p class="card-text">SMK SWASTA YAPIM SIAK HULU</p>
+    <a href="#" class="btn btn-primary">Beli</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Ulat German</h5>
+    <p class="card-text">SMK SWASTA YAPIM SIAK HULU</p>
+    <a href="#" class="btn btn-primary">Beli</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Talam Ubi Lumer</h5>
+    <p class="card-text">SMK SWASTA YAPIM SIAK HULU</p>
+    <a href="#" class="btn btn-primary">Beli</a>
+  </div>
+</div>
 @endsection
